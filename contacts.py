89a7d135a277c65b6b5e769944b4dbfaddb40ef4
oleksandr <oleksandr.i.vyrsta@ukd.edu.ua>
2026-03-30 22:27:45 +0300

    Change to dict + add delete

--- contacts.py
+++ contacts.py
@@ -1,19 +1,30 @@
-contacts = []
+contacts = {}
 
 def add_contact():
+    name = input("Введіть ПІБ: ")
     number = input("Введіть номер: ")
-    contacts.append(number)
+    contacts[name] = number
 
 def show_contacts():
-    for contact in contacts:
-        print(contact)
+    for name, number in contacts.items():
+        print(name, ":", number)
+
+def delete_contact():
+    name = input("Кого видалити: ")
+    if name in contacts:
+        del contacts[name]
 
 while True:
-    print("1 - Додати контакт")
-    print("2 - Показати контакти")
+    print("1 - Додати")
+    print("2 - Показати")
+    print("3 - Видалити")
+#Alpharius was here.
+
     choice = input("Вибір: ")
 
     if choice == "1":
         add_contact()
     elif choice == "2":
         show_contacts()
+    elif choice == "3":
+        delete_contact()
